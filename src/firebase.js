e02a7ac83ce694508446887febfa0b8160b155f3
Nicolas Dooley <nicolasdooley@gmail.com>
2026-07-07 22:18:08 -0400

    Add Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,18 +5,17 @@ import { getFirestore } from "firebase/firestore";
 // 2. Click the "</>" (web app) icon to register a web app, then copy the
 //    firebaseConfig object it gives you and paste it in place of the one below.
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID",
-};
-
+     apiKey: "AIzaSyBaiBGt9N649Wv2Uol_AfunSu4TPod-GLM",
+     authDomain: "movie-night-380d1.firebaseapp.com",
+     projectId: "movie-night-380d1",
+     storageBucket: "movie-night-380d1.firebasestorage.app",
+     messagingSenderId: "PASTE THE VALUE YOU SEE",
+     appId: "PASTE THE VALUE YOU SEE",
+   };
 // 2b. Pick a hard-to-guess room name only the two of you know.
 // This keeps strangers from stumbling onto your list even though the
 // site itself is public. Change this to something unique before deploying.
-export const ROOM_ID = "change-me-to-something-unique-abc123";
+export const ROOM_ID = "bubbles15";
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
